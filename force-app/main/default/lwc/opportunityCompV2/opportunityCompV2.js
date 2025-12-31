01072bc0bfa17230f83f112e87d5e7ca3968af8b
sagardodawad <dodawadsagar@gmail.com>
2025-12-31 14:59:51 +0530

    updated to expose true

---
+++ force-app/main/default/lwc/opportunityCompV2/opportunityCompV2.js
@@ -0,0 +1,26 @@
+import { LightningElement, wire} from 'lwc';
+import getWonOpportunities from '@salesforce/apex/OpportunityController.getWonOpportunities';
+
+export default class OpportunityCompV2 extends LightningElement {
+    // javascript class property
+   oppList;
+   @wire(getWonOpportunities)
+   loadWonDeals({data,error}){
+      if(data){  // if(data!=null)
+         //as a best practice
+         this.oppList = []; //define oppList as an empty array
+         // collections.forEach(function(col){});
+         data.forEach((opp)=>{
+            const obj = {
+                    Id: opp.Id,
+                    Name: opp.Name,
+                    Stage: opp.StageName,
+                    CloseDate: opp.CloseDate,
+                    Amount: opp.Amount,
+                    Commission: opp.Amount * 0.20        
+            };
+            this.oppList.push(obj);
+         });    
+      }
+   }
+}
